fb1a711b09cd08b33400cf7b5977bb6f4ed92f6d
education-bootcamp <education.seekerscloud@gmail.com>
2025-04-27 14:46:57 +0530

    `2`

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject, Inject} from '@angular/core';
+import {NgForOf} from '@angular/common';
+import {PostService} from '../../services/post.service';
 
 @Component({
   selector: 'app-order',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   standalone:true,
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
 export class OrderComponent {
+  postService = inject(PostService);
+  dataArray:any[]=[];
+  constructor() {
+    this.loadAllData();
+  }
 
+  private loadAllData() {
+    this.postService.loadAll().subscribe(response=>{
+      this.dataArray = response;
+    })
+  }
 }
--- src/app/pages/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NgForOf} from '@angular/common';
+import {PostService} from '../../services/post.service';
 
 @Component({
   selector: 'app-statistics',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   standalone:true,
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.scss'
 })
 export class StatisticsComponent {
+  postService = inject(PostService);
+  dataArray:any[]=[];
+  constructor() {
+    this.loadAllData();
+  }
 
+  private loadAllData() {
+    this.postService.loadAll().subscribe(response=>{
+      this.dataArray = response;
+    })
+  }
 }
